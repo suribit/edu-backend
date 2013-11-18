82d4e0bc76ac7ebaa6e413afbebff92bb062db26
Sergei Waribrus <wss.world@gmail.com>
2013-11-18 09:20:03 +0400

    fix bug

--- src/controllers/ProductController.php
+++ src/controllers/ProductController.php
@@ -13,21 +13,21 @@ class ProductController
     {
         $products = new ProductCollection([
             new Product([
-                'image'         => 'images/product.jpg',
+                'image'         => '/images/product.jpg',
                 'name'          => 'Nokla',
                 'sku'           => '1233212312312312',
                 'price'         => 100,
                 'special_price' => 99.99,
             ]),
             new Product([
-                'image'         => 'images/product.jpg',
+                'image'         => '/images/product.jpg',
                 'name'          => 'Nokla',
                 'sku'           => '1233212312312312',
                 'price'         => 100,
                 'special_price' => 99.99,
             ]),
             new Product([
-                'image'         => 'images/product.jpg',
+                'image'         => '/images/product.jpg',
                 'name'          => 'Nokla',
                 'sku'           => '1233212312312312',
                 'price'         => 100,
@@ -44,7 +44,7 @@ class ProductController
     public function viewAction()
     {
         $product = new Product([
-            'image'         => 'http://active-buy.com/foto/thumb-kopiya-nokia-6900-tv-2sim-fm-12-1mpx-kamera-mobilnye-telefony-597.jpg',
+            'image'         => '/images/product.jpg',
             'name'          => 'Nokla',
             'sku'           => '1233212312312312',
             'price'         => 100,
