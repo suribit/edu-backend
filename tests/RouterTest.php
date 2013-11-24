2b05b808f86877b0f710745e84a3f793d61db78c
Sergei Waribrus <wss.world@gmail.com>
2013-11-25 00:19:34 +0400

    homework sql file

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -5,17 +5,13 @@
  * @date     11/10/13
  */
 require_once __DIR__ . '/../src/models/Router.php';
+require_once __DIR__ . '/../src/models/PageNotFoundException.php';
 
 class RouterTest extends PHPUnit_Framework_TestCase
 {
     public function testReturnsControllerNameMatchedByRoute()
     {
-        $router = new Router('foo_bar');
-        $this->assertEquals(
-            'FooController', $router->getController()
-        );
-
-        $router = new Router('product_bar');
+        $router = new Router('product_list');
         $this->assertEquals(
             'ProductController', $router->getController()
         );
@@ -23,10 +19,34 @@ class RouterTest extends PHPUnit_Framework_TestCase
 
     public function testReturnsActionNameMatchedByRoute()
     {
-        $router = new Router('foo_bar');
-        $this->assertEquals('barAction',$router->getAction());
-
         $router = new Router('product_list');
         $this->assertEquals('listAction',$router->getAction());
+    }
+
+    /**
+     * @expectedException PageNotFoundException
+     * @expectedExceptionMessage Is not the same as the request format
+     */
+    public function testReturnsPageNotFoundNotSureTheFormat()
+    {
+        $router = new Router('ssss');
+    }
+
+    /**
+     * @expectedException PageNotFoundException
+     * @expectedExceptionMessage Controller file is not found
+     */
+    public function testReturnsPageNotFoundClassFileNotFound()
+    {
+        $router = new Router('sss_ss');
+    }
+
+    /**
+     * @expectedException PageNotFoundException
+     * @expectedExceptionMessage Will not find a method in a class
+     */
+    public function testReturnsPageNotFoundClassMethodNotFound()
+    {
+        $router = new Router('product_blablbla');
     }
 }
